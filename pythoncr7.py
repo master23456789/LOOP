44b7728f2c248680b57c56bfe25c2122e83ae80f
master23456789 <91898768+master23456789@users.noreply.github.com>
2021-12-17 18:43:59 +0530

    Add files via upload

---
+++ pythoncr7.py
@@ -0,0 +1,6 @@
+a = int(input('The first value:'))
+b = int(input('The second value:'))
+if a > b:
+    print("First number is greater than second")
+else:    
+    print("Second number is greater than first")
---
+++ unkownhpython.py
@@ -0,0 +1,7 @@
+a = int(input("What is the age of the user:"))
+b = 18
+if a > b:
+    print('The user has right to vote.')
+else:
+    print('The user has no right to vote.') 
+    
